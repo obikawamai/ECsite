e1f316a9502fdda08550483d3053923da7929c8b
obikawamai <ikem0605@icloud.com>
2020-03-30 17:37:16 +0900

    sql分のコメント一部消去

--- ecsite/sql/ecsite.sql
+++ ecsite/sql/ecsite.sql
@@ -43,5 +43,5 @@ delete_date datetime
 
 INSERT INTO item_info_transaction(item_name,item_price,item_stock)VALUES("ノートBook",100,50);
 
---INSERT INTO login_user_transaction(login_id,login_pass,user_name)VALUES("internous","internous01","test");
+INSERT INTO login_user_transaction(login_id,login_pass,user_name)VALUES("internous","internous01","test");
 
